78fd1d4dcb28d93788e64ab91f005b673ebb30b7
merkvilson <47288727+merkvilson@users.noreply.github.com>
2024-02-19 05:11:35 +0400

    Update ttt0.py

--- ttt0.py
+++ ttt0.py
@@ -1,7 +1,11 @@
 {
   "size": "13", 
-  "test": False
-
-
-
+  "test": False,
+  
+  "airen_engine": "https://drive.google.com/file/d/1Bfem9AIUY7YHb8DWN_Cwkd7sCw-ODDK2/view?usp=sharing", 
+  "plugin": "https://drive.google.com/file/d/1CCjnEB3i_w_VAWsrqGN10-FBOqdrltFk/view?usp=sharing",
+  "model": "https://drive.google.com/file/d/1OooGdLpAPypvTQawA1UHUG_VlEKGPO1h/view?usp=sharing",
+  "system": "https://drive.google.com/file/d/1V3SuddBtCdJHLClHBphf1tsZRyEFlbPQ/view?usp=sharing",
+  "webui": "https://drive.google.com/file/d/1eguKMl5IJcl3jC0t1wXJCtQudI86SmuR/view?usp=sharing"
+  
 }
