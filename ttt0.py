e34b6a7c7855fc2e5b37b59830fd5f86fc92d14c
merkvilson <47288727+merkvilson@users.noreply.github.com>
2024-02-22 00:20:43 +0400

    Update ttt0.py

--- ttt0.py
+++ ttt0.py
@@ -2,14 +2,14 @@
   "size": "13",
   "EULA": "MerkVilson Plugin: End User License Agreement\n\nThis EULA agreement and any dispute arising out of or in connection with this EULA agreement are governed by and construed in accordance with the laws.\n\nMerkVilson hereby grants you a licence to use the MerkVilson Plugin on your device in accordance with the terms of this EULA agreement.\n\nLicencing is PER PERSON and NOT per company. \nYou must purchase multiple licences for each user of the software.\n\nThis license is available non-exclusively, with no rights to copy, modify, merge, publish, distribute, sublicense, reverse engineering, sell, loan, rent, and/or lease copies of the Software. \nThe only exception can be made if MerkVilson directly grants you permission.\n\nThe software is provided \"as is\", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. \nIn no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.\n\n",
   "cover_link": "https://raw.githubusercontent.com/merkvilson/testdownload/main/aitex800.png",
-  "stable_link": {"link":"https://drive.google.com/file/d/1Bfem9AIUY7YHb8DWN_Cwkd7sCw-ODDK2/view?usp=sharing", "type":"7z"          },
+  "stable_link": {"link":"https://drive.google.com/file/d/1IbRKd6vx6wHM4KpBCEH3AK1rTnlKt07B/view?usp=sharing", "type":"7z"          },
   "webui_link":  {"link":"https://drive.google.com/file/d/1eguKMl5IJcl3jC0t1wXJCtQudI86SmuR/view?usp=sharing", "type":"zip"         },
   "system_link": {"link":"https://drive.google.com/file/d/1V3SuddBtCdJHLClHBphf1tsZRyEFlbPQ/view?usp=sharing", "type":"zip"         },
   "model_link":  {"link":"https://drive.google.com/file/d/1OooGdLpAPypvTQawA1UHUG_VlEKGPO1h/view?usp=sharing", "type":"safetensors"         },
   "plugin_link": {"link":"https://drive.google.com/file/d/1XavJgjuxwmR_7g0K4-uwEqlZxUdFF-qY/view?usp=sharing", "type":"zip" },
   "developer":  {"Merk": "https://merk.gumroad.com/"},
   "co_devs":    {"Wenhao Zhang":     "https://space.bilibili.com/35043394?spm_id_from=333.788.0.0",
-                "Ihar Krauchanka 123":  "https://www.linkedin.com/in/krachik/",
+                "Ihar Krauchanka":  "https://www.linkedin.com/in/krachik/",
                 "Nicol Substan":    "https://www.linkedin.com"},
   "finish": false
 }
