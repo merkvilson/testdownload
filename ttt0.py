67d93589a32f2dc070082b88a6f396ce598fee54
merkvilson <47288727+merkvilson@users.noreply.github.com>
2024-03-15 20:56:59 +0400

    Update ttt0.py

--- ttt0.py
+++ ttt0.py
@@ -1,6 +1,6 @@
-{
+ {
   "size": "13.7",
-  "version": 0.05,
+  "version": 0.06,
   "EULA": "MerkVilson Plugin: End User License Agreement\n\nThis EULA agreement and any dispute arising out of or in connection with this EULA agreement are governed by and construed in accordance with the laws.\n\nMerkVilson hereby grants you a licence to use the MerkVilson Plugin on your device in accordance with the terms of this EULA agreement.\n\nLicencing is PER PERSON and NOT per company. \nYou must purchase multiple licences for each user of the software.\n\nThis license is available non-exclusively, with no rights to copy, modify, merge, publish, distribute, sublicense, reverse engineering, sell, loan, rent, and/or lease copies of the Software. \nThe only exception can be made if MerkVilson directly grants you permission.\n\nThe software is provided \"as is\", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. \nIn no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.\n\n",
   
   "cover_link": "https://raw.githubusercontent.com/merkvilson/testdownload/main/aitex800.png",
@@ -68,8 +68,8 @@
              "progress": 0.94                                                                            ,
              "type": "safetensors"                                                                       },
             
-            {"link": "https://drive.google.com/file/d/1SgLtnJbJD6BhPqh8HVAeUIfy1_S0JilO/view?usp=sharing",
-             "name": "plugin_link__003"                                                                   ,
+            {"link": "https://drive.google.com/file/d/1-fIJkkfQURf9PRajJCDzVy4U8OFIn4dI/view?usp=sharing",
+             "name": "plugin_link__006"                                                                   ,
              "path": "P_PLUGINS"                                                                         ,
              "size": "0010"                                                                              ,
              "progress": 0.95                                                                            ,
